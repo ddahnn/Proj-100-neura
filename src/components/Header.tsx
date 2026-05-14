b57a9b08e778b03a060ddb4acab0b23a608fafe5
dahn <ddahnn@gmail.com>
2026-05-13 22:57:51 -0300

    menu bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,12 +16,14 @@ export const Header = () => {
             <p>
             No <span className="bold-text">100 neura</span>, a <span className="bold-text">tecnologia trabalha a favor da escuta.</span> Usamos soluções simples e atuais para <span className="bold-text">conectar pessoas a terapeutas que realmente façam sentido para sua história</span>, suas necessidades e seu momento de vida – sem complicar o que deveria ser cuidado.
             </p>
-          </div>
 
-          <ul>
-            <li><a href="#">Busco Terapia</a></li>
-            <li><a href="#">Sou Terapeuta</a></li>
-          </ul>
+            <div className="btn-hero">
+              <ul>
+                <li><a href="#" className='btn btn-primary'>Busco Terapia <img src="/src/assets/seta.png" alt="seta " className="hidden" /></a></li>
+                <li><a href="#" className='btn btn-secondary'>Sou Terapeuta <img src="/src/assets/seta.png" alt="seta " className="hidden" /></a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </header>
   );
--- src/components/parts/Nav.tsx
+++ src/components/parts/Nav.tsx
@@ -3,15 +3,18 @@ import Pesquisa from './pesquisar';
 export const HeaderNav = () =>  {
     return  (
         <nav>
-            <img src="src\assets\100-neura-logo-grande-escuro.png" alt="logo" />
+            <a href="#">
+                <img src="src\assets\100-neura-logo-grande-escuro.png" alt="logo" />
+            </a>
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Quem Somos</a></li>
                 <li><a href="#">Busco Terapia</a></li>
                 <li><a href="#">Sou Terapeuta</a></li>
                 <li><a href="#">Blog</a></li>
+                <li><a href="#" className="btn-login">Login <img src="../src/assets/seta.png" alt="seta " className="hid" /></a></li>
+                <li><a href="#" className="btn-cadastrar">Cadastrar <img src="https://www.figma.com/design/y2eL9Mkr7vAVRJHUCK3zdc/Arrows-Icons---Free--Community-?node-id=3-182&m=dev&t=v30cwqmy3JLUZM46-1" alt="seta " className="hid" style={{ height: '15px' }} /></a></li>
                 <li><Pesquisa/></li>
-
             </ul>
         </nav>
     )
